63a77cad8cc6e76c50a152bcf04b6354374d5324
diegolenci <lencidiego0@gmail.com>
2025-02-19 11:07:22 +0100

    correzione main e footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,55 @@ const Footer = () => {
     return (
         <footer>
             <section>
-               <h5>footer</h5>
-            </section>
             <div>
-                <a href="">SIGN-UP NOW!</a>
+                <ul>
+                    <li><a className="title" href="#">DC COMICS</a></li>
+                    <li><a href="#">Characters</a></li>
+                    <li><a href="#">Comics</a></li>
+                    <li><a href="#">Movies</a></li>
+                    <li><a href="#">Tv</a></li>
+                    <li><a href="#">Games</a></li>
+                    <li><a href="#">Videos</a></li>
+                    <li><a href="#">News</a></li>
+                </ul>
+                <ul>
+                    <li><a className="title" href="#">SHOP</a></li>
+                    <li><a href="#">Shop DC</a></li>
+                    <li><a href="#">Shop DC Collectibles</a></li>
+                </ul>
             </div>
+            <div>
+                <ul>
+                    <li><a className="title" href="#">DC</a></li>
+                    <li><a href="#">Terms Of Use</a></li>
+                    <li><a href="#">Privacy policy(new)</a></li>
+                    <li><a href="#">Ad Choices</a></li>
+                    <li><a href="#">Advertising</a></li>
+                    <li><a href="#">Jobs</a></li>
+                    <li><a href="#">Subscriptions</a></li>
+                    <li><a href="#">Talent Workshops</a></li>
+                    <li><a href="#">CPSC Certificate</a></li>
+                    <li><a href="#">Ratings</a></li>
+                    <li><a href="#">Shop Help</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li><a className="title" href="#">SITES</a></li>
+                    <li><a href="#">DC</a></li>
+                    <li><a href="#">MAD Magazine</a></li>
+                    <li><a href="#">DC Kids</a></li>
+                    <li><a href="#">DC Universe</a></li>
+                    <li><a href="#">DC Power Visa</a></li>
+                </ul>
+            </div>
+            </section>
+            
+            
+            <nav>
+                <a href="">SIGN-UP NOW!</a>
+            </nav>
         </footer>
     )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,15 +4,28 @@ const Main = () => {
             <section>
                 <h2>Content goes here</h2>
             </section>
-            <div>
-                <ul>
-                    <li><a href=""></a>DIGITAL COMICS</li>
-                    <li><a href=""></a>DC MERCHANDISE</li>
-                    <li><a href=""></a>SUBSCRIPTION</li>
-                    <li><a href=""></a>COMIC SHOP LOCATOR</li>
-                    <li><a href=""></a>DC POWER VISA</li>
-                </ul>
-            </div>
+            <nav>
+                <div>
+                    <img src="src/assets/img/buy-comics-digital-comics.png" alt="" width="45px" height="50px"/>
+                    <p>DIGITAL COMICS</p>
+                </div>
+                <div>
+                    <img src="src/assets/img/buy-comics-merchandise.png" alt="" width="45px" height="50px"/>
+                    <p>DC MERCHANDISE</p>
+                </div>
+                <div>
+                    <img src="src/assets/img/buy-comics-subscriptions.png" alt="" width="45px" height="50px"/>
+                    <p>SUBSCRIPTION</p>
+                </div>
+                <div>
+                    <img src="src/assets/img/buy-comics-shop-locator.png" alt="" width="40px" height="50px"/>
+                    <p>SHOP LOCATOR</p>
+                </div>
+                <div>
+                    <img src="src/assets/img/buy-dc-power-visa.svg" alt="" width="40px" height="50px"/>
+                    <p>POWER VISA</p>
+                </div>
+            </nav>
         </main>
     )
 }
