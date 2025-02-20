a44b76b6562ff8789f92e4ea3122ebba2f2e6ed0
diegolenci <lencidiego0@gmail.com>
2025-02-20 17:05:22 +0100

    correzione layout delle card

--- src/components/Comics.jsx
+++ src/components/Comics.jsx
@@ -61,7 +61,7 @@ const Comics = () => {
           id: 5,
           title: "Batman #56",
           description: "The Dark Knight's looking to drop both the hammer and sickle on the KGBeast. The Russian super-assassin has gone too far, and Batman will stop at nothing to hunt him down. But is the Dark Knight willing to step into the darkness himself to find justice?",
-          thumb: "https://imgs.search.brave.com/3vP5d3cOVOuC0f6Uhm7CkV_qFk5hGj5rE4jK5I6n7dI/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9jZG4y/LnBlbmd1aW4uY29t/LmF1L2NvdmVycy9v/cmlnaW5hbC83OTYx/OTQzMTM5OTk4Lmpw/Zw",
+          thumb: "https://imgs.search.brave.com/jgxYlrx442aozemzjqdmCsd9DkIkRollJp1T8sG8TfE/rs:fit:720:1106:1/g:ce/aHR0cHM6Ly9veXN0/ZXIuaWduaW1ncy5j/b20vd29yZHByZXNz/L3N0Zy5pZ24uY29t/LzIwMTgvMTAvU1RM/MDk3MDk1LTcyMHgx/MTA2LmpwZw",
           price: "$3.99",
           series: "Batman",
           sale_date: "2018-10-03",
@@ -162,8 +162,8 @@ const Comics = () => {
                 comics.map((product) => {
                     return (
                         <div className="card" key={product.id}>
-                            <img src={product.thumb} alt="" height="100" width="10 e 0"/>
-                            <h3>{product.title}</h3>
+                            <img src={product.thumb} alt="" height="150" width="140"/>
+                            <h3 className="card-subtitle">{product.title}</h3>
                         </div>
                     )
                 })
